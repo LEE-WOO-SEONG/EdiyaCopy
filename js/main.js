3c457c76dc1ad721f1643a5466cb5c5b7e9956b1
mari301509 <lmr301509@gmail.com>
2020-04-08 20:38:32 +0900

    css merge

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
+var menuOpen = $('.btn-openMenu');
+var menuClose = $('.btn-closeMenu');
+var drinkOpen = $('.btn-infoOpen');
+var drinkClose = $('.btn-infoClose');
 
-// 변수
-var btnInfoOpen = $('.btn-infoOpen');
-var btnInfoClose = $('.btn-infoClose');
 
+menuOpen.click(function(e) {
+    e.preventDefault(); /* anchor 이벤트 발생억제 */
+    $(this).parent().removeClass('nav-none');
+    $(this).parent().addClass('nav-act');
+}
+);
 
+menuClose.click(function(e) {
+    e.preventDefault(); /* anchor 이벤트 발생억제 */
+    $(this).parent().parent().removeClass('nav-act'); /* 해당 class name을 모두 지우고 */
+    $(this).parent().parent().addClass('nav-none');
+}
+);
 
-//funtion
-btnInfoOpen.click(function(event){
-    event.preventDefault();
-    $(this).parent().addClass('drink-infoOn');
-})
-btnInfoClose.click(function(event){
-    event.preventDefault();
-    $(this).parent().parent().removeClass('drink-infoOn');
-})
+drinkOpen.click(function(e) {
+    e.preventDefault(); /* anchor 이벤트 발생억제 */
+    $(this).parent().addClass('drink-act');
+}
+);
+
+drinkClose.click(function(e) {
+    e.preventDefault(); /* anchor 이벤트 발생억제 */
+    $(this).parent().parent().removeClass('drink-act'); /* 해당 class name을 모두 지우고 */ 
+}
+);
