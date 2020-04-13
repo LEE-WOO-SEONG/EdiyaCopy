600f3ce557ae55f0800cda71d6e8655084f5e39a
LEE-WOO-SEONG <akakqogk@gmail.com>
2020-04-13 14:56:35 +0900

    feat:modifying markup as the instructor's feedback.
    Adding attribute 'role' / 'aria-modal' / 'aria-haspopup' /
    'aria-pressed'.
    The 'aria-pressed' value's true or false are given by using jquery.

--- js/main.js
+++ js/main.js
@@ -19,13 +19,17 @@ menuClose.click(function(e) {
 );
 
 drinkOpen.click(function(e) {
-    e.preventDefault(); /*  이벤트 발생억제 */
+    e.preventDefault();                        /*  이벤트 발생억제 */
     $(this).parent().addClass('drink-act');
+    $(this).removeAttr('aria-pressed');        /* 버튼의 눌림상태 해제 */
+    $(this).attr('aria-pressed','true');       /* 버튼의 눌림상태 추가 */
 }
 );
 
 drinkClose.click(function(e) {
     e.preventDefault(); /* 이벤트 발생억제 */
-    $(this).parent().parent().removeClass('drink-act'); /* 해당 class name을 모두 지우고 */ 
+    $(this).parent().parent().removeClass('drink-act');                  /* 해당 class name을 모두 지우고 */ 
+    $(this).parent().prev(drinkOpen).removeAttr('aria-pressed');         /* 버튼의 눌림상태 해제 */
+    $(this).parent().prev(drinkOpen).attr('aria-pressed','false');       /* 버튼의 눌림상태 추가 */
 }
 );
